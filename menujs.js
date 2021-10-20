f52857c2562534c6fda7637c94777b62f2e171cb
Michael Zhi Yang <myang124@umd.edu>
2021-10-19 22:17:06 -0400

    added buttons

---
+++ menujs.js
@@ -0,0 +1,10 @@
+
+console.log(window.localStorage.getItem("first time"));
+
+if(window.localStorage.getItem("first time") != 1){
+    window.localStorage.setItem("first time", 1);
+}
+
+console.log(window.localStorage.getItem("first time"));
+//user prompt to ask first time usres for name/goal/gender
+//window.localStorage.setItem("first time", 0);
