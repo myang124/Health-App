42deed2f615183d806bb8ed2aa115c5450a14c84
csheng05 <csheng05@gmail.com>
2021-10-29 01:02:37 -0400

    Progress page mostly complete

---
+++ ProgressScreen/progress_script.js
@@ -0,0 +1,51 @@
+/*
+    Cynthia SHeng
+*/
+
+function displayWeight() {
+    var w = document.getElementById("weightStat");
+    var wg = document.getElementById("weightGraph");
+    var c = document.getElementById("calorieStat");
+    var cg = document.getElementById("calorieGraph");
+    var m = document.getElementById("macroStat");
+    var mg = document.getElementById("macroChart");
+
+    w.style.display = "block";
+    wg.style.display = "block"
+    c.style.display = "none";
+    cg.style.display = "none";
+    m.style.display = "none";
+    mg.style.display = "none";
+}
+
+function displayCalorie() {
+    var w = document.getElementById("weightStat");
+    var wg = document.getElementById("weightGraph");
+    var c = document.getElementById("calorieStat");
+    var cg = document.getElementById("calorieGraph");
+    var m = document.getElementById("macroStat");
+    var mg = document.getElementById("macroChart");
+
+    w.style.display = "none";
+    wg.style.display = "none"
+    c.style.display = "block";
+    cg.style.display = "block";
+    m.style.display = "none";
+    mg.style.display = "none";
+}
+
+function displayMacro() {
+    var w = document.getElementById("weightStat");
+    var wg = document.getElementById("weightGraph");
+    var c = document.getElementById("calorieStat");
+    var cg = document.getElementById("calorieGraph");
+    var m = document.getElementById("macroStat");
+    var mg = document.getElementById("macroChart");
+
+    w.style.display = "none";
+    wg.style.display = "none"
+    c.style.display = "none";
+    cg.style.display = "none";
+    m.style.display = "block";
+    mg.style.display = "block";
+}
