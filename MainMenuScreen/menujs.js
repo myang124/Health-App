d6d6197b7ae1d7a4766afd2e1e48ab5756270cc4
Thomas Le <40319997+DoctorLe@users.noreply.github.com>
2021-11-02 15:40:49 -0400

    Worked on main menu Thomas

--- MainMenuScreen/menujs.js
+++ MainMenuScreen/menujs.js
@@ -9,3 +9,6 @@ if(window.localStorage.getItem("first time") != 1){
 console.log(window.localStorage.getItem("first time"));
 //user prompt to ask first time usres for name/goal/gender
 //window.localStorage.setItem("first time", 0);
+
+
+//Ask user for information once
