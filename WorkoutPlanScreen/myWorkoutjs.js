bd7e823c89f68dde5ad192c04b87ed073274c74d
Michael Zhi Yang <myang124@umd.edu>
2021-11-05 10:38:09 -0400

    almost done with functions, just need to include a few more task scenerios

--- WorkoutPlanScreen/myWorkoutjs.js
+++ WorkoutPlanScreen/myWorkoutjs.js
@@ -8,7 +8,31 @@ function removeChecked(){
     var list = document.querySelectorAll('input[type="checkbox"]:checked');
     
     for(var index = 0; index < list.length; index++){
-        console.log(list[index].parentElement.textContent);
+        var target = list[index].parentElement.textContent;
+        for(var t = 0; t < myList.length; t++){
+            if(target == myList[t]){
+                myList.splice(t,1);
+            }
+        }
     }
+    localStorage[localStorage.getItem("day") + "list"] = JSON.stringify(myList);
+    viewList();
 }
 
+function viewList(){
+            //js to display workouts
+            var ul = document.getElementById("checklist");
+
+            ul.innerHTML = "";
+
+            for(var index = 0; index < myList.length; index++){
+    
+                var li = document.createElement("li");
+                li.innerHTML = myList[index];
+                var input = document.createElement("input");
+                input.type = "checkbox";
+
+                ul.appendChild(li);
+                li.appendChild(input);
+            }
+}
